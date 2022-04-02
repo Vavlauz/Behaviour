58180d67a2a734ccfeec44fead74ccdcb052c0a7
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-04-02 15:25:11 +0700

    Initial commit

---
+++ src/test/java/ru/netology/domain/test/MoneyTransferTest.java
@@ -0,0 +1,41 @@
+package ru.netology.domain.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.data.DataHelper;
+import ru.netology.domain.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.testng.AssertJUnit.assertEquals;
+import static ru.netology.domain.data.DataHelper.getFirstCardInfo;
+import static ru.netology.domain.data.DataHelper.getSecondCardInfo;
+
+public class MoneyTransferTest {
+
+    @BeforeEach
+    public void setUp() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verifyInfo);
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        int amount = 1000;
+        var expectedBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo) - amount;
+        var expectedBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo) + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+}
