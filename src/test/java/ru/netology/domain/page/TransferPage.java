cc2da5170ca65d4880922a13429bc858030e101a
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-04-03 01:02:07 +0700

    Initial commit

--- src/test/java/ru/netology/domain/page/TransferPage.java
+++ src/test/java/ru/netology/domain/page/TransferPage.java
@@ -1,11 +1,13 @@
 package ru.netology.domain.page;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
 import ru.netology.domain.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static org.openqa.selenium.Keys.*;
 
 public class TransferPage {
     private SelenideElement transferButton = $("[data-test-id='action-transfer']");
@@ -19,6 +21,15 @@ public class TransferPage {
 
     public DashboardPage makeTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
         amountInput.setValue(amountToTransfer);
+        fromInput.setValue(cardInfo.getCardNumber());
+        transferButton.click();
+        return new DashboardPage();
+    }
+
+    public DashboardPage makeTransfer2(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountInput.sendKeys(chord(SHIFT,HOME,BACK_SPACE));
+        amountInput.setValue(amountToTransfer).sendKeys(TAB);
+        fromInput.sendKeys(Keys.BACK_SPACE);
         fromInput.setValue(cardInfo.getCardNumber());
         transferButton.click();
         return new DashboardPage();
